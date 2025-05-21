4158507c3bc23a5b50e3d9660a523f35c2d75c30
Fathur <fatur300904@gmail.com>
2025-05-21 07:39:43 +0700

    fix: update models/index.js

--- src/models/index.js
+++ src/models/index.js
@@ -13,7 +13,13 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),
+    port: 3306,
+    logging: false,
+  });
 }
 
 fs
