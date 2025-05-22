9869e0a9cdc63f271c0196f9686b0f2e678dcbc6
Fathur <fatur300904@gmail.com>
2025-05-22 16:15:28 +0700

    fix: update prd routes

--- src/routes/prd.routes.js
+++ src/routes/prd.routes.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const prdController = require('../controllers/prd.controller');
-const authMiddleware = require('../middlewares/auth.middleware');
 
-router.use(authMiddleware);
 router.get('/', prdController.getAllPRDs);
 router.get('/:id', prdController.getPRDById);
 router.post('/', prdController.createPRD);
